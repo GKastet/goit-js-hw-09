800bcebd0ba46b77500180d62e4f94a6f2ebbb2a
GKastet <127600582+GKastet@users.noreply.github.com>
2023-06-15 22:48:03 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const elements = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+elements.btnStop.disabled = true;
+
+elements.btnStart.addEventListener('click', handlerBtnStartClick);
+
+function handlerBtnStartClick() {
+  timerId = setInterval(() => {
+    elements.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  elements.btnStart.disabled = true;
+  elements.btnStop.disabled = false;
+}
+
+elements.btnStop.addEventListener('click', handlerBtnStopClick);
+function handlerBtnStopClick() {
+  clearInterval(timerId);
+  elements.btnStart.disabled = false;
+  elements.btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
