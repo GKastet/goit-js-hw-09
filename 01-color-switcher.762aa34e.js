7136567bec6b368d45695704f290cd3c81ebbb25
GKastet <127600582+GKastet@users.noreply.github.com>
2023-06-26 08:08:00 +0000

    Deploying to gh-pages from @ GKastet/goit-js-hw-09@3e0fac3391e891f07c3387afe83bd2b0c7de9e61 🚀

--- 01-color-switcher.762aa34e.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};let e;t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.762aa34e.js.map
